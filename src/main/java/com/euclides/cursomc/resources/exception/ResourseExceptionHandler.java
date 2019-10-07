c87148808bd6b648f955eac62ad6b461f87de269
Euclides Mendonça <euclidesduarte5@gmail.com>
2019-10-07 16:59:02 -0100

    Deletando Categoria com DELETE

--- src/main/java/com/euclides/cursomc/resources/exception/ResourseExceptionHandler.java
+++ src/main/java/com/euclides/cursomc/resources/exception/ResourseExceptionHandler.java
@@ -2,6 +2,7 @@ package com.euclides.cursomc.resources.exception;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,11 +12,20 @@ import javassist.tools.rmi.ObjectNotFoundException;
 @ControllerAdvice
 public class ResourseExceptionHandler {
 	
+	
 	public ResponseEntity<StandartError> ObjectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 		
 		StandartError err = new StandartError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		
+	}
+	
+	public ResponseEntity<StandartError> dataIntegrity(DataIntegrityViolationException e, HttpServletRequest request){
+		
+		StandartError err = new StandartError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 		
 	}
 
--- src/main/java/com/euclides/cursomc/services/CategoriaService.java
+++ src/main/java/com/euclides/cursomc/services/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.euclides.cursomc.domain.Categoria;
@@ -33,6 +34,17 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException("Não é possivel excluir uma categoria que possui produtos");
+		}
+		
 	}
 
 }
